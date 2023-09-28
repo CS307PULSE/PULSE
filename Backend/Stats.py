ba3c59387d3a60369f2e93cebf94b284470fb51b
Nbs0327 <82853280+Nbs0327@users.noreply.github.com>
2023-09-28 09:48:11 -0400

    Added updating recent songs and followed artists
    :D

--- Backend/Stats.py
+++ Backend/Stats.py
@@ -1,3 +1,7 @@
 class Stats:
-    def __init__():
-        return
+    def __init__(self,
+                 recent_songs=None,
+                 followed_artists=None):
+        self.recent_songs = recent_songs
+        self.followed_artists = followed_artists
+
--- Backend/User.py
+++ Backend/User.py
@@ -9,23 +9,48 @@ class Theme(Enum):
 
 class User:
     def __init__(self, 
-                 displayName="",
-                 loginToken="", 
+                 display_name="",
+                 login_token="", 
                  userID="", 
+                 spotify_user=None,
                  icon=None, 
-                 friendsList=None, 
+                 friends_list=None, 
                  playlists=None, 
                  theme=Theme.DARK, 
-                 AllStats=None, 
-                 highScores=None, 
-                 recommendationParams=None):
-        self.displayName = displayName                                                                  # String
-        self.loginToken = loginToken                                                                    # String
-        self.userID = userID                                                                            # String
-        self.icon = icon                                                                                # Icon
-        self.friendsList = friendsList if friendsList is not None else []                               # Array of userID Strings
-        self.playlists = playlists if playlists is not None else []                                     # Array of Playlists
-        self.theme = theme                                                                              # Theme
-        self.AllStats = AllStats if AllStats is not None else {}                                        # Dict<Key:String,Value:Stats>
-        self.highScores = highScores if highScores is not None else []                                  # Array of Int
-        self.recommendationParams= recommendationParams if recommendationParams is not None else []     # Array of Doubles
+                 stats=None, 
+                 high_scores=None, 
+                 recommendation_params=None):
+        self.display_name = display_name                                                                   # String
+        self.login_token = login_token                                                                     # String
+        self.userID = userID                                                                               # String
+        self.spotify_user = spotify_user                                                                   # Spotify User
+        self.icon = icon                                                                                   # Icon
+        self.friends_list = friends_list if friends_list is not None else []                               # Array of userID Strings
+        self.playlists = playlists if playlists is not None else []                                        # Array of Playlists
+        self.theme = theme                                                                                 # Theme
+        self.stats = stats if stats is not None else Stats()                                               # Stats
+        self.high_scores = high_scores if high_scores is not None else []                                  # Array of Int
+        self.recommendation_params= recommendation_params if recommendation_params is not None else []     # Array of Doubles
+
+    # Updates list of recent songs with at most 50 objects of type Track
+    def updateRecentSongs(self):
+        self.stats.recent_songs = self.spotify_user.current_user_recently_played()['items']
+    
+    # Updates list of followed artists with at most max_artists number of objects of type Artist
+    def updateFollowedArtists(self,max_artists=500):
+        followed_artists = []
+
+        after = None
+        limit = min(50, max_artists)
+
+        while len(followed_artists) < max_artists:
+            artists = self.spotify_user.current_user_followed_artists(limit=limit, after=after)['artists']
+
+            after = artists['cursors']['after']
+
+            followed_artists.extend(artists['items'])
+
+            if not artists['items'] or after is None:
+                break
+
+        self.stats.followed_artists = followed_artists
