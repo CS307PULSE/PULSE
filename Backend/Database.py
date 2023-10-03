8efa1c70632b70228ff5bf9b887de1df9330d420
Nbs0327 <82853280+Nbs0327@users.noreply.github.com>
2023-10-03 12:09:31 -0400

    Firebase server!
    Zero chance this works

--- Backend/Database.py
+++ Backend/Database.py
@@ -3,10 +3,55 @@ from User import User
 from User import Theme
 import json
 from types import SimpleNamespace
+import os
 
 class DBHandling:
+    def erase():
+        db_file = os.getcwd() + '\\Backend\\' + 'DBTempStorage.txt'
+        open(db_file, 'w').close()
+
+    def temp_is_user_in_DB(spotify_id):
+        db_file = os.getcwd() + '\\Backend\\' + 'DBTempStorage.txt'
+        # Check if a user with the given Spotify ID exists in the database
+        with open(db_file, 'r') as file:
+            for line in file:
+                user_data = json.loads(line)
+                if user_data["spotify_id"] == spotify_id:
+                    return True
+        return False
+
+    def temp_is_user_in_DB(spotify_id):
+        db_file = os.getcwd() + '\\Backend\\' + 'DBTempStorage.txt'
+        # Check if a user with the given Spotify ID exists in the database
+        with open(db_file, 'r') as file:
+            for line in file:
+                user_data = json.loads(line)
+                if user_data.get("spotify_id") == spotify_id:
+                    return True
+        return False
+
+    def temp_store_user_in_DB(user):
+        # Store a user in the database file
+        db_file = os.getcwd() + '\\Backend\\' + 'DBTempStorage.txt'
+        with open(db_file, 'a') as file:
+            user_data = user.to_json()
+            user_json = json.dumps(user_data)
+            file.write(user_json + '\n')
+
+    def temp_get_user_from_DB(spotify_id):
+        # Retrieve a user by Spotify ID from the database
+        db_file = os.getcwd() + '\\Backend\\' + 'DBTempStorage.txt'
+        with open(db_file, 'r') as file:
+            for line in file:
+                user_data = json.loads(line)
+                user = User.from_json(user_data)
+                if user.spotify_id == spotify_id:
+                    return user
+        return None
+
     #gets and returns an entire serialized user from the DB
     def get_user_from_DB(spotify_id):
+        return DBHandling.temp_get_user_from_DB(spotify_id)
         try:
             # Connect to database
             connection = mysql.connector.connect(host="pulse-sql-server.mysql.database.azure.com",
@@ -78,6 +123,7 @@ class DBHandling:
     
     #TODO: Ensure user doesn't already exist
     def store_new_user_in_DB(newUser):
+        return DBHandling.temp_store_user_in_DB(newUser)
         try:
             # Connect to database
             connection = mysql.connector.connect(host="pulse-sql-server.mysql.database.azure.com",
@@ -102,31 +148,7 @@ class DBHandling:
                                                  json.dumps(newUser.friends),
                                                  int(newUser.theme.value),
                                                  json.dumps(newUser.highscores),
-                                                 json.dumps(newUser.recommendation),)
-            record = cursor.fetchall()
-        except mysql.connector.Error as error:
-            print("Failed to read user data from MySQL table {}".format(error))
-
-        finally:
-            if connection.is_connected():
-                cursor.close()
-                connection.close()
-                #print("MySQL connection is closed")
-        return None
-    
-    def update_token(spotify_id, login_token):
-        try:
-            # Connect to database
-            connection = mysql.connector.connect(host="pulse-sql-server.mysql.database.azure.com",
-                                                user="pulse_prototype_users",
-                                                password="thisPasswordMeansNothing!!!",
-                                                database="pulse")
-            cursor = connection.cursor()
-
-            # Update login_token
-            sql_fetch_blob_query = """UPDATE pulse.users SET login_token = %s WHERE spotify_id = %s """
-            
-            cursor.execute(sql_fetch_blob_query, (login_token, spotify_id,))
+                                                 json.dumps(newUser.recommendation))
             record = cursor.fetchall()
         except mysql.connector.Error as error:
             print("Failed to read user data from MySQL table {}".format(error))
@@ -141,7 +163,8 @@ class DBHandling:
     def update_user(user, field):
         return None
     
-    def does_user_exist_in_DB(spotify_id):        
+    def does_user_exist_in_DB(spotify_id):
+        return DBHandling.temp_is_user_in_DB(spotify_id)
         try:
             # Connect to database
             connection = mysql.connector.connect(host="pulse-sql-server.mysql.database.azure.com",
