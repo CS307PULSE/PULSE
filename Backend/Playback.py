18a7b21e633f3e8994f7304bbf8c778d00fbcae4
dmshaffe <dmshaffe@purdue.edu>
2023-10-02 20:59:40 -0400

    Add multiprocess functionality and current playback pulling

--- Backend/Playback.py
+++ Backend/Playback.py
@@ -1,26 +1,50 @@
 import spotipy
-import subprocess
+import multiprocessing
 import time
 import User
+from Exceptions import ErrorHandler
 
 class Playback:
-    def init(self,devices,shuffle,repeat, currentlyplaying):
-        devices = get_devices()
+    def __init__(self, user = None):
+        self.user = user
+        self.devices = ""
+        self.currently_playing = ""
+        self.shuffle = ""
+        self.repeat = ""
+        self.current_device = ""
+        self.progress = ""
+        self.is_playing = ""
 
-    def begin_player():
-       while True:
-        playback_info = get_current_playback()
-        print("Current Playback:", playback_info)
-    
-        time.sleep(1)
-
-    def get_current_playback():
+    def initial_check(self):
         try:
-         result = subprocess.run(["current_playback"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, timeout=1)
-         if result.returncode == 0:
-            return result.stdout.strip()
-         else:
-            return "Error: Command failed"
-        except subprocess.TimeoutExpired:
-            return "Error: Timeout"
+            self.devices = self.user.spotify_user.devices()
+            self.currently_playing = self.user.spotify_user.current_playback()
+            self.is_playing = self.currently_playing['is_playing']
+            if self.is_playing : 
+               self.shuffle = self.currently_playing['shuffle_state']
+               self.repeat = self.currently_playing['repeat_state']
+               self.current_device = self.currently_playing['device']
+               self.progress = self.currently_playing['progress_ms']
+            else : 
+               self.progress = 0
+        except spotipy.exceptions.SpotifyException as e:
+            ErrorHandler.handle_error(e)
 
+    def player_checker(self):
+       try:
+        while True:
+            self.currentlyplaying = self.user.spotify_user.get_current_playback()
+            print("Current Playback:", playback_info)
+            time.sleep(1)
+       except spotipy.exceptions.SpotifyException as e:
+        ErrorHandler.handle_error(e)
+
+    def start_thread(self):
+        checker = multiprocessing.Process(target = player_checker, args=())
+        checker.start()
+
+    def set_shuffle(self):
+        if self.shuffle :
+           self.user.spotify_user.shuffle(False)
+        else :
+           self.user.spotify_user.shuffle(True)
