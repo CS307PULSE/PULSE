6bd4d51310a8364c62472dc881301f79d5434193
Cryotts <bodhiscott@gmail.com>
2023-10-03 17:13:37 -0400

    Update userTableCreation.sql
    updated reference text

--- Database/userTableCreation.sql
+++ Database/userTableCreation.sql
@@ -10,8 +10,8 @@ CREATE TABLE IF NOT EXISTS pulse.users(
     display_name VARCHAR(65) NOT NULL,                           -- PULSE display name
     login_token JSON,                                            -- Spotify login token in their format
     spotify_id varChar(200) NOT NULL UNIQUE,                     -- spotify username (always unique)
-    friends JSON                                                 -- "Friend1,Friend2,Friend3...." (String)
-    high_scores JSON,                                            -- "Score1,Score2,Score3..." (String but individual scores can be converted to ints)
+    friends varChar(5000),                                                 -- "Friend1,Friend2,Friend3...." (String)
+    high_scores varChar(200),                                            -- "Score1,Score2,Score3..." (String but individual scores can be converted to ints)
     theme INT(2) ZEROFILL NOT NULL,                              -- Chosen theme. Defaults to 0 (dark mode)
-    recommendation_params JSON                                   --  "Stat1,Stat2,Stat3,Stat4..." (String but individual stats can be converted to doubles)
+    recommendation_params varChar(200)                                   --  "Stat1,Stat2,Stat3,Stat4..." (String but individual stats can be converted to doubles)
 );
