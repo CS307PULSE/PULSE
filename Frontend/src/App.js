9eb1180e4bfba6c2b6408dc7edbdf52fb723d17f
RheaaSharma <rheaasharma@gmail.com>
2023-10-05 18:57:16 -0400

    Update App.js
    connection issue

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,10 +1,33 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Login from "./components/Login";
 
+import { Navigate,  BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Dashboard from "./components/Mainpage"
+
+import axios from "axios";
+
+async function fetchData() {
+  const response = await axios.get("http://localhost:5000/");
+  const data = await response.json();
+  return data;
+}
 
 function App() {
   const [isLoginClicked, setLoginClicked] = useState(false);
+  //check for the cache value and if it already exsists 
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  
+  //check using backend if the cache has a user value stored in it 
+  useEffect(() => {
+    fetchData().then(data => {
+      console.log("unicorn")
+      if (data === "T") {
+        setIsLoggedIn(true);
+        console.log(data)
+      }
+    });
+  }, []);
 
   const handleLoginClick = () => {
     // Perform any login logic here if needed
@@ -12,10 +35,21 @@ function App() {
     setLoginClicked(true);
   };
   return (
-    <div>
-    {isLoginClicked ? null : <Login onLoginClick={handleLoginClick} />}
-    <div></div>
-  </div>
+    <Router>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          isLoggedIn ? (
+            <Navigate to="/dashboard" />
+          ) : (
+            <Login onLoginClick={handleLoginClick} />
+          )
+        }
+      />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  </Router>
   );
 }
 
