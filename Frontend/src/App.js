2656499c88634fdf611739ccb2345c9c8fb0d542
RheaaSharma <rheaasharma@gmail.com>
2023-10-05 21:51:13 -0400

    Connecting frontend and backend
    Frontend code for connection of app.js and login.js with Flask framework

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState, useEffect } from "react";
+import { useState, useEffect, } from "react";
 import Login from "./components/Login";
 
 import { Navigate,  BrowserRouter as Router, Route, Routes } from "react-router-dom";
@@ -7,40 +7,47 @@ import Dashboard from "./components/Mainpage"
 
 import axios from "axios";
 
-async function fetchData() {
-  const response = await axios.get("http://localhost:5000/");
-  const data = await response.json();
+//--------------------------------Pending: cookies check and reture userID to backend 
+// for the cache and DB call for fetch
+async function fetchDataCache() {
+  const response = await axios.get("http://127.0.0.1:5000/");
+  const data = response.data;
+  console.log(data)
   return data;
 }
 
+
 function App() {
   const [isLoginClicked, setLoginClicked] = useState(false);
-  //check for the cache value and if it already exsists 
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  //check for the cache value and if it already exsists : From backend 
+  const [isCacheDB, setIsCacheDB] = useState(false);
+  
   
   //check using backend if the cache has a user value stored in it 
   useEffect(() => {
-    fetchData().then(data => {
-      console.log("unicorn")
+    fetchDataCache().then(data => {
       if (data === "T") {
-        setIsLoggedIn(true);
-        console.log(data)
+        setIsCacheDB(true);
       }
     });
   }, []);
 
+
+
+
   const handleLoginClick = () => {
+
     // Perform any login logic here if needed
     // For now, just set the state to true to indicate the button is clicked
     setLoginClicked(true);
   };
   return (
-    <Router>
+  <Router>
     <Routes>
       <Route
         path="/"
         element={
-          isLoggedIn ? (
+          isCacheDB ? (
             <Navigate to="/dashboard" />
           ) : (
             <Login onLoginClick={handleLoginClick} />
