bd3d1657f1053cbd1e3c01bff4c33c37e10902d2
Jason <zheng669@purdue.edu>
2023-10-07 19:28:22 -0400

    Added pie graph
    Included pie graph into sample graphs

--- StatisticsPage/src/Graphs.js
+++ StatisticsPage/src/Graphs.js
@@ -1,5 +1,6 @@
 import { ResponsiveBar } from "@nivo/bar";
 import { ResponsiveLine } from "@nivo/line";
+import { ResponsivePie } from "@nivo/pie";
 
 const data = [
   {
@@ -305,6 +306,39 @@ const data2 = [
   },
 ];
 
+const data3 = [
+  {
+    id: "java",
+    label: "java",
+    value: 195,
+    color: "hsl(90, 70%, 50%)",
+  },
+  {
+    id: "erlang",
+    label: "erlang",
+    value: 419,
+    color: "hsl(56, 70%, 50%)",
+  },
+  {
+    id: "ruby",
+    label: "ruby",
+    value: 407,
+    color: "hsl(103, 70%, 50%)",
+  },
+  {
+    id: "haskell",
+    label: "haskell",
+    value: 474,
+    color: "hsl(186, 70%, 50%)",
+  },
+  {
+    id: "go",
+    label: "go",
+    value: 71,
+    color: "hsl(104, 70%, 50%)",
+  },
+];
+
 export const BarGraph = () => {
   return (
     <ResponsiveBar
@@ -398,5 +432,26 @@ export const LineGraph = () => (
     ]}
   />
 );
+
+export const Pie = () => {
+  return (
+    <ResponsivePie
+      data={data3}
+      margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+      innerRadius={0.5}
+      padAngle={0.7}
+      cornerRadius={3}
+      activeOuterRadiusOffset={8}
+      borderWidth={1}
+      borderColor={{ from: "color", modifiers: [["darker", 0.2]] }}
+      arcLinkLabelsSkipAngle={10}
+      arcLinkLabelsTextColor="#333333"
+      arcLinkLabelsThickness={2}
+      arcLinkLabelsColor={{ from: "color" }}
+      arcLabelsSkipAngle={10}
+      arcLabelsTextColor={{ from: "color", modifiers: [["darker", 2]] }}
+    />
+  );
+};
 
 export default BarGraph;
