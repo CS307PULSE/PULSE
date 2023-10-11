2a747acbaf00047666dc8d9f19e8033a457be8e5
Nbs0327 <82853280+Nbs0327@users.noreply.github.com>
2023-10-11 19:45:42 -0400

    WOOOO FIXES

--- Backend/DatabaseConnector.py
+++ Backend/DatabaseConnector.py
@@ -101,7 +101,8 @@ class DatabaseConnector(object):
                                                 new_user.spotify_id,
                                                 create_friends_string_for_DB(new_user.friends),
                                                 int(new_user.theme.value),
-                                                create_highscore_string_for_DB(new_user.high_scores),
+                                                new_user.location,
+                                                new_user.gender,
                                                 create_rec_params_string_for_DB(new_user.recommendation_params),))
 
         self.db_conn.commit()
