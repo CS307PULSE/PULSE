3c1a1658cef6ab340561a6bd48e8a0e3fb48a7f3
Cryotts <bodhiscott@gmail.com>
2023-10-22 14:31:12 -0400

    Update FriendsCard.js
    Displays friend components on the omnipresent FriendCards

--- Frontend/src/components/FriendsCard.js
+++ Frontend/src/components/FriendsCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Friend from './Friend'; 
 import styled from 'styled-components';
 import { Link } from "react-router-dom";
 
@@ -8,7 +9,7 @@ import axios from "axios";
 import Colors from "../theme/Colors"; 
 import TextSize from "../theme/TextSize";
 
-var textSizeSetting, themeSetting;
+var textSizeSetting, themeSetting, friends;
 try {
     var textSizeResponse = await axios.get("http://127.0.0.1:5000/get_text_size", {withCredentials: true});
     textSizeSetting = textSizeResponse.data;
@@ -19,19 +20,39 @@ try {
     textSizeSetting = 1;
     themeSetting = 0;
 }
+
+try {
+  var friendsResponse = await axios.get("http://127.0.0.1:5000/get_friends", {withCredentials: true});
+  friends = friendsResponse.data;
+
+} catch (e) {
+  console.log("Friends fetch failed: " + e);
+  friends = [];
+}
+
 const themeColors = Colors(themeSetting); //Obtain color values
 const textSizes = TextSize(textSizeSetting); //Obtain text size values
+const friendsList = friends;
 
 // Styled components
 const CardContainer = styled.div`
   border: 1px solid ${themeColors.white};
-  overflow: hidden;
-  width: 500px;
+  overflow-y: auto;
+  max-width: 500px; /* Set a maximum width to limit the container size */
+  width: 100%; /* Ensure the container takes available width */
   height: 650px;
+  margin: 0 auto; /* Horizontally center the container */
+`;
+
+// Add margin to each Friend component for spacing
+const FriendContainer = styled.div`
+  margin-bottom: 20px; /* Adjust the margin as needed for spacing */
 `;
 
 const Header = styled.div`
   background-color: ${themeColors.green}; // Set background color to green
+  top: 0;
+  position: sticky;
   color: black; // Set text color to green
   font-family: 'Poppins', sans-serif;
   font-size: 14px;
@@ -52,17 +73,70 @@ text-transform: uppercase;
   padding: 16px;
 `;
 const StyledLink = styled(Link)`
+  position: sticky;
   text-decoration: none; /* Remove underline */
   color: inherit; /* Inherit text color */
 `;
 
-const FriendsCard = ({children }) => {
+
+const FriendsCard = ({}) => {
+  const friendData = [
+    {
+      name: 'John Doe',
+      photoUri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1024px-Default_pfp.svg.png',
+      favoriteSong: 'Bohemian Rhapsody',
+    },
+    {
+      name: 'Jane Smith',
+      photoUri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1024px-Default_pfp.svg.png',
+      favoriteSong: 'Hotel California',
+    },
+    {
+      name: 'Jane Smith',
+      photoUri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1024px-Default_pfp.svg.png',
+      favoriteSong: 'Hotel California',
+    },
+    {
+      name: 'Jane Smith',
+      photoUri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1024px-Default_pfp.svg.png',
+      favoriteSong: 'Hotel California',
+    },
+    {
+      name: 'Jane Smith',
+      photoUri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1024px-Default_pfp.svg.png',
+      favoriteSong: 'Hotel California',
+    },
+    {
+      name: 'Jane Smith',
+      photoUri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1024px-Default_pfp.svg.png',
+      favoriteSong: 'Hotel California',
+    },
+    {
+      name: 'Jane Smith',
+      photoUri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1024px-Default_pfp.svg.png',
+      favoriteSong: 'Hotel California',
+    },
+    // Add more friend data as needed
+  ];
+  
     return (
       <CardContainer>
         <StyledLink to="/friends">
-        <Header>FRIENDS</Header>
-      </StyledLink>
-        <Content>{children}</Content>
+          <Header>FRIENDS</Header>
+        </StyledLink>
+        
+        <div className="friend-list">
+          {friendData.map((friend, index) => (
+            <FriendContainer key={index}>
+              <Friend
+                name={friend.name}
+                photoFilename={friend.photoUri}
+                favoriteSong={friend.favoriteSong}
+              />
+            </FriendContainer>
+          ))}
+        </div>
+      
       </CardContainer>
     );
   }
