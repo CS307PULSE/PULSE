8ff251ec3313870b9e49ef5b96d34d78b6e911d5
Cryotts <bodhiscott@gmail.com>
2023-10-26 19:55:41 -0400

    Update FriendRequests.js

--- Frontend/src/components/FriendRequests.js
+++ Frontend/src/components/FriendRequests.js
@@ -1,4 +1,6 @@
 import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import Friend from './Friend';
 import Navbar from "./NavBar";
 import Colors from "../theme/Colors";
 import TextSize from "../theme/TextSize";
@@ -33,15 +35,165 @@ const bodyStyle = {
   justifyContent: "space-between",
 };
 
+const contentStyle = {
+  flex: 1, // Ensure content takes available space
+  padding: "16px",
+  overflow: "auto",
+};
+
+
+const buttonStyle = {
+  backgroundColor: themeColors.background,
+  color: themeColors.text,
+  padding: "20px 40px", // Increase the padding for taller buttons
+  borderWidth: "1px",
+  borderStyle: "solid",
+  borderColor: themeColors.text,
+  borderRadius: "10px",
+  cursor: "pointer",
+  margin: "5px",
+  width: "100%", // Adjust the width to take up the entire space available
+  textAlign: "center", // Center the text horizontally
+};
+
+const friendRowStyle = {
+  display: "grid",
+  gridTemplateColumns: "repeat(auto-fill, minmax(150px, 1fr))", // Adjust column width as needed
+  gap: "10vw", // Adjust the gap between friends
+  marginBottom: "16px", // Add vertical space between rows
+  alignItems: "center", // Align items vertically in the center
+  textAlign: "center", // Center text horizontally
+};
+
+const buttonContainerStyle = {
+  display: "flex",
+  justifyContent: "center", // Center buttons horizontally
+  marginBottom: "16px",
+};
+
 const FriendRequests = () => {
-  useEffect(() => {
-    document.title = "PULSE - Friends";
-  }, []);
+    //DELETE WHEN CONNECTING TO BACKEND
+  const requestsData = [
+    {
+      name: 'John Doe',
+      spotify_id: 'test',
+      photoUri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1024px-Default_pfp.svg.png',
+      favoriteSong: 'Bohemian Rhapsody',
+    },
+    {
+      name: 'Jane Smith',
+      spotify_id: 'test',
+      photoUri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1024px-Default_pfp.svg.png',
+      favoriteSong: 'Hotel California',
+    },
+    {
+      name: 'Jane Smith',
+      spotify_id: 'test',
+      photoUri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1024px-Default_pfp.svg.png',
+      favoriteSong: 'Hotel California',
+    },
+    {
+      name: 'Jane Smith',
+      spotify_id: 'test',
+      photoUri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1024px-Default_pfp.svg.png',
+      favoriteSong: 'Hotel California',
+    },
+    {
+      name: 'Jane Smith',
+      spotify_id: 'test',
+      photoUri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1024px-Default_pfp.svg.png',
+      favoriteSong: 'Hotel California',
+    },
+    {
+      name: 'Jane Smith',
+      spotify_id: 'test',
+      photoUri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1024px-Default_pfp.svg.png',
+      favoriteSong: 'Hotel California',
+    },
+    {
+      name: 'Jane Smith',
+      spotify_id: 'test',
+      photoUri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1024px-Default_pfp.svg.png',
+      favoriteSong: 'Hotel California',
+    },
+    // Add more friend data as needed
+  ];
+  async function friendRequestDenied(spotify_id) {
+    console.log("Friend request denied: " + spotify_id)
+    alert("Friend request denied!");
+    return
+    /* const axiosInstance = axios.create({
+      withCredentials: true,
+    });
+    const response = await axiosInstance.post(
+      "http://127.0.0.1:5000/friends/friend_request_denied",
+      { denial: sentDential }
+    );
+    const data = response.data;
+    console.log("Friend request denied: " + sentDenial);
+    return data;
+    */
+  }
+
+  async function friendRequestAccepted(spotify_id) {
+    console.log("Friend request accepted: " + spotify_id)
+    alert("Friend request accepted!");
+    return
+    /* const axiosInstance = axios.create({
+      withCredentials: true,
+    });
+    const response = await axiosInstance.post(
+      "http://127.0.0.1:5000/friends/friend_request_accepted",
+      { acceptance: sentAcceptance }
+    );
+    const data = response.data;
+    console.log("Friend request accepted: " + sentAcceptance);
+    return data;
+    */
+  }
+
+  const renderRequestRows = () => {
+    const rows = [];
+    for (let i = 0; i < requestsData.length; i += 5) {
+      const requestsInRow = requestsData.slice(i, i + 5);
+      rows.push(
+        <div key={i} style={friendRowStyle}>
+          {requestsInRow.map((friend, index) => (
+            <div key={friend.name + index}>
+              <Friend
+                name={friend.name}
+                photoFilename={friend.photoUri}
+                favoriteSong={friend.favoriteSong}
+              />
+              <div class = "center">
+                <button onClick={() => friendRequestDenied(friend.spotify_id)}>
+                  Deny
+                </button>
+                <button onClick={() => friendRequestAccepted(friend.spotify_id)}>
+                  Accept
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      );
+    }
+    return rows;
+  };
+
+  const noRequestsMessage = (
+    <div style={{ textAlign: "center", marginTop: "20px" }}>
+      <p>You have no pending friend requests</p>
+    </div>
+  );
 
   return (
     <div style={bodyStyle}>
       <Navbar />
-      <div>Not implemented yet! Sorry.</div>
+      <div style={buttonContainerStyle}>
+          <Link to="/friends" style={{ ...buttonStyle, textDecoration: 'none' }}>Back</Link>
+      </div>
+      {requestsData ? (requestsData.length > 0 ? renderRequestRows() : noRequestsMessage) : noRequestsMessage}
     </div>
   );
 };
