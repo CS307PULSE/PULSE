7567f163466dcb7290566b1549e7b6292d721919
RheaaSharma <rheaasharma@gmail.com>
2023-11-03 16:13:23 -0400

    Update DatabaseConnector.py

--- Backend/DatabaseConnector.py
+++ Backend/DatabaseConnector.py
@@ -80,8 +80,7 @@ class DatabaseConnector(object):
                                                 create_friends_string_for_DB(new_user.friends),
                                                 int(new_user.theme.value),
                                                 new_user.location,
-                                                new_user.gender,
-                                                create_rec_params_string_for_DB(new_user.recommendation_params),))
+                                                new_user.gender,))
 
             self.db_conn.commit()
             affected_rows = self.db_cursor.rowcount
@@ -341,7 +340,6 @@ class DatabaseConnector(object):
                          spotify_id=row[3],                                                             
                          friends=create_friends_array_from_DB(row[4]),        
                          theme=Theme(row[5]),                                                         
-                         recommendation_params=create_rec_params_string_for_DB(row[7]),
                          location = row[9],
                          gender = row[10],
                          chosen_song = row[15],)       
