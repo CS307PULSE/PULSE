79caebb0bfafa6d12a121e4286cd1b11e579972d
Cryotts <bodhiscott@gmail.com>
2023-11-18 15:52:18 -0600

    Update main.py

---
+++ main.py
@@ -0,0 +1,3 @@
+from Backend.API import app                                                               
+if __name__ == "__main__":                                             
+    app.run(debug=True)
