4b769b9c0491cae0fcb99e83b62bcaa6275ec669
Drak0420 <zheng669@purdue.edu>
2023-11-25 17:52:37 -0500

    Fixed stats card

--- Frontend/src/components/Mainpage.js
+++ Frontend/src/components/Mainpage.js
@@ -90,77 +90,6 @@ function Mainpage() {
     return data;
   }
 
-  function StatsCardComp() {
-    //Data to display on stats card
-    const [topArtists, setTopArtists] = useState();
-    const [topSongs, setTopSongs] = useState();
-    const [followers, setFollowers] = useState();
-    const [statsDone, setStatsDone] = useState(false);
-
-    useEffect(() => {
-      updateFollowers();
-      const fetchData = async () => {
-        const response = await axios.get("/statistics/short", {
-          withCredentials: true,
-        });
-        console.log("response to stats card");
-        console.log(response);
-        const data = response.data;
-        try {
-          setTopArtists(JSON.parse(data.top_artists));
-        } catch (e) {
-          console.log(e);
-          console.log("Top Artist empty");
-        }
-
-        try {
-          setTopSongs(JSON.parse(data.top_songs));
-        } catch (e) {
-          console.log("Top Song empty");
-        }
-        if (data.follower_data === "") {
-          console.log("Followers empty");
-        } else {
-          const dateObjects = Object.keys(data.follower_data);
-          const lastObj =
-            data.follower_data[dateObjects[dateObjects.length - 1]];
-          console.log(dateObjects[dateObjects.length - 1]);
-          setFollowers(lastObj);
-        }
-        setStatsDone(true);
-      };
-
-      fetchData();
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-
-    if (!statsDone) {
-      return <p>Loading</p>;
-    }
-    try {
-      return (
-        <>
-          <p style={cardContent}>
-            Favorite artist of all time: {topArtists[2][0].name}
-          </p>
-          <p style={cardContent}>
-            Favorite artist recently: {topArtists[0][0].name}
-          </p>
-          <p style={cardContent}>
-            Favorite song of all time: {topSongs[2][0].name}
-          </p>
-          <p style={cardContent}>
-            Favorite song recently: {topSongs[0][0].name}
-          </p>
-          <p style={cardContent}>You have {followers} followers</p>
-        </>
-      );
-    } catch (e) {
-      console.log(e);
-      return <p>Try refreshing, data was bad, sorry!</p>;
-    }
-  }
-
   const handleChatbotClick = () => {
     alert("Chatbot clicked!");
   };
@@ -176,7 +105,7 @@ function Mainpage() {
       <div className="content" style={bodyStyle}>
         <div style={cardContainerStyle}>
           <Card headerText="STATISTICS" style={cardStyle}>
-            {StatsCardComp()}
+            <StatsCard />
           </Card>
           <Card headerText="DJ MIXER" style={cardStyle}>
             <p style={cardContent}>This is the content of Card 2.</p>
